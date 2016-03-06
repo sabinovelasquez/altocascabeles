6a65d0de55ab5098296408ef12de3ab936be6317
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2016-03-06 13:41:07 -0300

    top structure

--- source/js/all.js
+++ source/js/all.js
@@ -5,6 +5,8 @@
 //=require vendor/angular/angular-ui-bootstrap-modal
 //=require vendor/angular/angular-parallax
 //=require vendor/angular/angular-scroll.min
+//=require vendor/angular/angular-touch.min
+
 //=require vendor/ui-bootstrap-tpls-0.14.3.min
 //=require vendor/angulartics
 //=require vendor/angulartics-google-analytics
