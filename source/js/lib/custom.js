60efd68f331512fcdf53507fef293388976b38dc
Sabino Velasquez <sabino@Sabinos-MacBook-Pro.local>
2016-03-11 20:49:36 -0300

    modal lotes

--- source/js/lib/custom.js
+++ source/js/lib/custom.js
@@ -2,8 +2,25 @@
 var app = angular
 
 	.module('app', ['ngAnimate','ui.bootstrap', 'ngMap', 'ngTouch', 'angular-parallax', 'duScroll', 'angulartics', 'angulartics.google.analytics'])
-	// .controller('headerCtrl', [ '$scope',
-	// 	function($scope) {
+	.controller('mapCtrl', [ '$scope', '$uibModal',
+		function($scope, $uibModal) {
+			$scope.open = function (size) {
 
-	// 	}
-	// ])
+				var modalInstance = $uibModal.open({
+					templateUrl: 'modalTemplate',
+					controller: 'ModalInstanceCtrl',
+					size: 'lg'
+				});
+			}
+		}
+	])
+	.controller('ModalInstanceCtrl', ['$scope', '$uibModalInstance',
+		function ($scope, $uibModalInstance) {
+
+			$scope.cancel = function () {
+				$uibModalInstance.dismiss('cancel');
+			};
+		}
+
+	]);
+
